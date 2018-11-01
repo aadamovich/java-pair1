33f779c4b2d53ebca7e1aef17f8ffafa3e9be27a
EE\toomasu <toomas.uttendorf@playtech.com>
2018-11-01 10:32:45 +0200

    Adding new stuff

--- src/com/playtech/Main.java
+++ src/com/playtech/Main.java
@@ -3,8 +3,9 @@ package com.playtech;
 public class Main {
 
     public static void main(String[] args) {
-      System.out.println("Hello, World!");
-      System.out.println("Yeah!");
-      ///
+        System.out.println("Hello, World!");
+        System.out.println("Yeah!");
+        ///
+        System.out.println("Let's change things");
     }
 }
