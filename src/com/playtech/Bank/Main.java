f3fb66e457d4fcf407487ca92b0b718d89cc6229
margusm <margus.moru@gmail.com>
2018-11-01 14:20:51 +0200

    TODO

--- src/com/playtech/Bank/Main.java
+++ src/com/playtech/Bank/Main.java
@@ -3,5 +3,10 @@ package com.playtech.Bank;
 public class Main {
     public static void main(String[] args) {
 
+        //TODO Matrix for numbrs
+        //TODO HashMap for numberMatrix vs number
+        //TODO Recognize numbers
+        //TODO Validate numbers
+        //TODO Write to file
     }
 }
